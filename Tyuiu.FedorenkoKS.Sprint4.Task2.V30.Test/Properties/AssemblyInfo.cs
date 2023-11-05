f5e91550691572137d5208714d0b5b41ede9955c
Tyuiu.FedorenkoKS@outlook.com <kirilll.fedorenko.2020@mail.ru>
2023-11-05 21:08:25 +0300

    Add project files.

---
+++ Tyuiu.FedorenkoKS.Sprint4.Task2.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.FedorenkoKS.Sprint4.Task2.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.FedorenkoKS.Sprint4.Task2.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2221804e-3fbf-41f1-a9f6-319a85de7abc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
